5dea80b2eeaf856afbb62842f8e5357a9598f4bc
Shawn Zernik <szernik@noemail>
2024-12-09 00:33:13 +0000

    nvm

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #! /bin/sh
 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install 20
+node -v
+npm -v
+
 set -e # set to exit if non-zero return value
 
 set +x
