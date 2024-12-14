4644638f171bd3379b647bf43280e7b7582ec33b
Shawn Zernik <>
2024-12-13 21:04:50 -0600

    restored changes

--- terraform/terraform/network.tf
+++ terraform/terraform/network.tf
@@ -1,102 +1,19 @@
-resource "aws_vpc" "cloudroute_network" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
+module "lvt-root" {
+  source = "./lvt_network"
 
-  tags = {
-    Name = "cloudroute-vpc"
-  }
-}
+  vpc_cidr_block = var.VPC_CIDR
+  vpc_name       = var.VPC_NAME
 
-resource "aws_internet_gateway" "cloudroute_network_igw" {
-  vpc_id = aws_vpc.cloudroute_network.id
+  sn1_name              = var.SN1_NAME
+  sn1_cidr_block        = var.SN1_CIDR
+  sn1_availability_zone = var.SN1_AZ
 
-  tags = {
-    Name = "cloudroute-vpc-igw"
-  }
-}
+  sn2_name              = var.SN2_NAME
+  sn2_cidr_block        = var.SN2_CIDR
+  sn2_availability_zone = var.SN2_AZ
 
-resource "aws_subnet" "cloudroute_network_sn1" {
-  vpc_id                  = aws_vpc.cloudroute_network.id
-  cidr_block              = "10.0.1.0"
-  availability_zone       = "us-west-2a"
-  map_public_ip_on_launch = false
+  region = var.REGION
 
-  tags = {
-    Name = "cloudroute-vpc-sn1"
-  }
-}
-
-resource "aws_route_table" "cloudroute_network_sn1_rt" {
-  vpc_id = aws_vpc.cloudroute_network.id
-
-  tags = {
-    Name = "cloudroute-vpc-sn1-rt"
-  }
-}
-
-resource "aws_route" "cloudroute_network_sn1_rt_internet" {
-  route_table_id         = aws_route_table.cloudroute_network_sn1_rt.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.cloudroute_network_igw.id
-}
-
-resource "aws_route" "cloudroute_network_sn1_rt_vpn" {
-  route_table_id         = aws_route_table.cloudroute_network_sn1_rt.id
-  destination_cidr_block = "10.0.0.0/24"
-  network_interface_id   = module.cloudroute.instance_primary_network_interface_id
-}
-
-resource "aws_route_table_association" "cloudroute_network_sn1_rta" {
-  subnet_id      = aws_subnet.cloudroute_network_sn1.id
-  route_table_id = aws_route_table.cloudroute_network_sn1_rt.id
-}
-
-resource "aws_subnet" "cloudroute_network_sn2" {
-  vpc_id                  = aws_vpc.cloudroute_network.id
-  cidr_block              = "10.0.2.0"
-  availability_zone       = "us-west-2c"
-  map_public_ip_on_launch = false
-
-  tags = {
-    Name = "cloudroute-vpc-sn2"
-  }
-}
-
-resource "aws_route_table" "cloudroute_network_sn2_rt" {
-  vpc_id = aws_vpc.cloudroute_network.id
-
-  tags = {
-    Name = "cloudroute-vpc-sn2-rt"
-  }
-}
-
-resource "aws_route" "cloudroute_network_sn2_rt_internet" {
-  route_table_id         = aws_route_table.cloudroute_network_sn2_rt.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.cloudroute_network_igw.id
-}
-
-resource "aws_route" "cloudroute_network_sn2_rt_vpn" {
-  route_table_id         = aws_route_table.cloudroute_network_sn2_rt.id
-  destination_cidr_block = "10.0.0.0/24"
-  network_interface_id   = module.cloudroute.instance_primary_network_interface_id
-}
-
-resource "aws_route_table_association" "cloudroute_network_sn2_rta" {
-  subnet_id      = aws_subnet.cloudroute_network_sn2.id
-  route_table_id = aws_route_table.cloudroute_network_sn2_rt.id
-}
-
-resource "aws_vpc_endpoint" "cloudroute_root_vpc_s3" {
-  vpc_id       = aws_vpc.cloudroute_network.id
-  service_name = "com.amazonaws.us-west-2.s3"
-  route_table_ids = [
-    aws_route_table.cloudroute_network_sn1_rt.id,
-    aws_route_table.cloudroute_network_sn2_rt.id
-  ]
-
-  tags = {
-    Name = "cloudroute-vpc-ep"
-  }
+  instance_network_interface_id = module.cloudroute.instance_primary_network_interface_id
+  openvpn_cidr                  = var.OPENVPN_CIDR
 }
